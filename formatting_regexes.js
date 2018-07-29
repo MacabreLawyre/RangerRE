7e5e84ae0e345c8989e90fa8c0cf0443edfe955f
Ross Macfarlane <rmacfarlane@transitionaldata.com>
2018-07-28 21:08:50 -0400

    a lot of conditional reformatting and variable renaming

--- formatting_regexes.js
+++ formatting_regexes.js
@@ -35,6 +35,7 @@ while (true) {
 	z = x && (y > k)
 }
 // ##############################################################
+
 // ##############################################################
 // FIX INDENTING AFTER FIXING SHOULD-BE IF STATEMENTS
 // ##############################################################
@@ -63,6 +64,7 @@ while (true) {
 	c = 3;>>>>
 }
 // ##############################################################
+
 // ##############################################################
 // PART 2
 // ##############################################################
@@ -91,6 +93,7 @@ while (true) {
 	c = 3;>>>>
 }
 // ##############################################################
+
 // ##############################################################
 // INDENT FUNCTIONS
 // ##############################################################
@@ -99,3 +102,73 @@ while (true) {
 //REPLACE
 $1\n$2
 // ##############################################################
+
+// ##############################################################
+// EXPAND TERNARY OPS
+// ##############################################################
+//FIND
+(\t*)(.*?)([a-zA-Z0-9_]+) (>|>=|==|<=|<) ([a-zA-Z0-9_]+) \? (.+) : ([^:\?\n;]+)
+(\t*)(.*[\t ])(([a-zA-Z0-9_]+)( (>|>=|==|<=|<) ([a-zA-Z0-9_]+))?) \? (.+?) : ([^:\?\n;]+)
+(\t*)(.*[\t ])(([a-zA-Z0-9_]+)( (>|>=|==|<=|<) ([a-zA-Z0-9_]+))?) \? (.+?) : ([^:\?\n;]+)
+\n(\t*)([^:\n]*[\t ]\?*\(?)([a-zA-Z0-9_]+ (>|>=|==|<=|<) ([a-zA-Z0-9_]+)|([a-zA-Z0-9_]+)) *\? *(.+?) *: *([^:\?\n;]+)
+//REPLACE
+$1$2($3 $4 $5)\n$1\t? ($6)\n$1\t: ($7)
+{$1$2}($3)\n$1\t? ($8)\n$1\t: ($9)
+$1$2($3) ? ($8) t: ($9)
+$1$2#if($3){$8}else{$9}#
+//EXAMPLE BEFORE
+var condition1 = true,
+	condition2 = false,
+	access = condition1 ? condition2 ? "true true": "true false" : condition2 ? "false true" : "false false";
+	access = condition1 ? (condition2 ? "true true": "true false") : (condition2 ? "false true" : "false false");
+
+	condition2 ? "false true" : "false false";
+	
+function ig(a, b) {
+	return a > b ? a : b
+}
+
+function K(a, b, c) {
+	return a < b ? b : a > c ? c : a
+}
+function egg(a, b) {
+	for (var a = ea * fa - 1; 0 <= a; a--)
+		C[a] = 0;
+	9 >= f ? uf() : 49 >= f ? vf() : 59 >= f ? wf() : 60 <= f && xf();
+	if (0 < ge)
+		ge--,
+		yf(D, 480, 247, "save OK", 16711680, 6684672);
+	if (0 < ke)
+		ke--,
+		2 == je ? D.b(10, 172) : 3 == je ? D.b(10, 1084672) : 4 == je && D.b(1072);
+}
+// EXAMPLE AFTER
+var condition1 = true,
+	condition2 = false,
+	access =  (IF(condition1)THEN{(IF(condition2)THEN{"true true"}ELSE{"true false" })}ELSE{(IF(condition2)THEN{"false true"}ELSE{"false false"}) }) ;
+	access =  (IF(condition1)THEN{(condition2 ? "true true"}ELSE{"true false") }) : (condition2 ? "false true" : "false false");
+
+	 (IF(condition2)THEN{"false true"}ELSE{"false false"}) ;
+	
+function ig(a, b) {
+	return a >  (IF(b)THEN{a}ELSE{b}) 
+}
+
+function K(a, b, c) {
+	return a <  (IF(b)THEN{b}ELSE{a >  (IF(c)THEN{c}ELSE{a}) }) 
+}
+function egg(a, b) {
+	for (var a = ea * fa - 1; 0 <= a; a--)
+		C[a] = 0;
+	9 >=  (IF(f)THEN{uf()}ELSE{49 >=  (IF(f)THEN{vf()}ELSE{59 >=  (IF(f)THEN{wf()}ELSE{60 <= f && xf()}) }) }) ;
+	if (0 < ge)
+		ge--,
+		yf(D, 480, 247, "save OK", 16711680, 6684672);
+	if (0 < ke)
+		ke--,
+		2 ==  (IF(je)THEN{D.b(10, 172)}ELSE{3 ==  (IF(je)THEN{D.b(10, 1084672)}ELSE{4 == je && D.b(1072)}) }) ;
+}
+// ##############################################################
+
+
+
